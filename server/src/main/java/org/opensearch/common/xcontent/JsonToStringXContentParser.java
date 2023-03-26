247d6851f1d1622ae2e0886183fc4b0ee2aa204f
Yaliang Wu <ylwu@amazon.com>
2023-03-25 17:50:13 -0700

    fix breaking changes
    Signed-off-by: Yaliang Wu <ylwu@amazon.com>

--- server/src/main/java/org/opensearch/common/xcontent/JsonToStringXContentParser.java
+++ server/src/main/java/org/opensearch/common/xcontent/JsonToStringXContentParser.java
@@ -33,12 +33,7 @@ package org.opensearch.common.xcontent;
 
 import org.opensearch.common.bytes.BytesReference;
 import org.opensearch.common.xcontent.json.JsonXContent;
-import org.opensearch.core.xcontent.AbstractXContentParser;
-import org.opensearch.core.xcontent.DeprecationHandler;
-import org.opensearch.core.xcontent.NamedXContentRegistry;
-import org.opensearch.core.xcontent.XContentBuilder;
-import org.opensearch.core.xcontent.XContentLocation;
-import org.opensearch.core.xcontent.XContentParser;
+import org.opensearch.common.xcontent.support.AbstractXContentParser;
 import org.opensearch.index.mapper.ParseContext;
 import java.io.IOException;
 import java.nio.CharBuffer;
